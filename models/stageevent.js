832a000aab2fce3c5dbe75204f50bc3e1fd3127b
Bachir <bachirmdicko@gmail.com>
2022-10-20 19:17:41 -0400

    Remaining Controllers Created

--- models/stageevent.js
+++ models/stageevent.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'StageEvent',
-    tableName: 'stage_events',
+    tableName: 'stages_events',
     timestamps: false,
   })
   return StageEvent
